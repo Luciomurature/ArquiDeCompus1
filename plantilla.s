94ef343c04ea4c4127beddb37685499227b15bc7
matischroder <37404887+matischroder@users.noreply.github.com>
2019-06-18 16:55:56 -0300

    Update plantilla.s

--- plantilla.s
+++ plantilla.s
@@ -6,17 +6,34 @@
 
 .global main
 main:
-      LDR R3, =array // load base address of a into R3
-      ... more code here
+      LDR R3, = array // load base address of a into R3
+      loop:
+          MOV R0,#16
+          SUBS R0,R0,#1
+          LD outpu
+          
+      BL 
 done: NOP            // dummy instruction for breakpoint instruccion boba, que no hace nada
       BX LR          // return from main
 
 .data
 array:
-      .byte 0x08
+      .byte 0x80
+      .byte 0x80
+      .byte 0x40
+      .byte 0x40
+      .byte 0x20
+      .byte 0x20
       .byte 0x10
-      .byte 0xFF
-      ... more data here
+      .byte 0x10
+      .byte 0x88
+      .byte 0x48
+      .byte 0x24
+      .byte 0x14
+      .byte 0x0A
+      .byte 0x06
+      .byte 0x03
+      .byte 0x01
 .end
 
 
