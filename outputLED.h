2da9907786f1e73e1071b48c53b198951c0b545e
luciomurature <luciomurature@gmail.com>
2019-06-18 13:43:43 -0300

    easyPIO y ouputLED xd

---
+++ outputLED.h
@@ -0,0 +1,18 @@
+#ifndef outputLED_h
+#define outputLED_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ncurses.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include "EasyPIO.h"
+
+
+void outputLED(unsigned char);
+
+
+
+
+#endif
